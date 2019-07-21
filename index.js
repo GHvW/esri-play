9129a6b5a2f8922dbfde943e4fc9c8746191e801
Garrett van Wageningen <ghvanw283@gmail.com>
2019-07-20 21:49:23 -0500

    a commit with stuff

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+// look into these for vue:
+// https://github.com/oppoudel/CreateMap
+// https://github.com/tomwayson/esri-vue-cli-example
+// https://github.com/odoe/vue-jsapi4
+
 require([
   "esri/Map",
-  "esri/views/MapView"
-], function (Map, MapView) {
+  "esri/views/MapView",
+  "esri/layers/VectorTileLayer",
+  "esri/Basemap",
+], function (Map, MapView, Basemap, VectorTileLayer) {
+
+  // const awesomeBasemap = new Basemap({
+  //   baselayers: [
+  //     new VectorTileLayer({ // https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-VectorTileLayer.html
+  //       style: styling // pulls in from vTileLayer via import order in index.html til modules are figured out
+  //     })
+  //   ]
+  // });
 
   const map = new Map({
-    basemap: "topo-vector"
+    basemap: "streets-navigation-vector"
   });
 
   const view = new MapView({ // 2D
