9d61bdb5b9ba2d367bdb4139ee3fab60957175d2
Garrett van Wageningen <ghvanw283@gmail.com>
2019-07-20 23:57:22 -0500

    add notes

--- index.js
+++ index.js
@@ -3,6 +3,18 @@
 // https://github.com/tomwayson/esri-vue-cli-example
 // https://github.com/odoe/vue-jsapi4
 
+
+// for Feature Layer practice https://developers.arcgis.com/javascript/latest/guide/add-layers-to-a-map/
+// feature layer for client side graphics https://developers.arcgis.com/javascript/latest/sample-code/layers-featurelayer-collection/index.html
+// query feature layer
+// apply edits to feature layer
+// use featureform layer to allow users to apply edits https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-FeatureForm.html
+
+// graphics layer https://developers.arcgis.com/javascript/latest/api-reference/esri-layers-GraphicsLayer.html 
+
+// instead of graphics layer for updates, should we just cache the featureforms / graphics / geometries or whatever for any given session
+
+
 require([
   "esri/Map",
   "esri/views/MapView",
